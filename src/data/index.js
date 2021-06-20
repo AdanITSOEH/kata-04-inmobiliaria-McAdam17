a14bdcf6292d73500506ed6503166bebc94f66eb
Adan Mota Cruz <adanmotacruz17@gmail.com>
2021-06-20 15:05:09 -0500

    Add Data The example data was added and implemented in HomeScreen

---
+++ src/data/index.js
@@ -0,0 +1,93 @@
+const images = [
+  require('../assets/house_1.jpg'),
+  require('../assets/house_2.jpg'),
+  require('../assets/house_3.jpg'),
+  require('../assets/house_4.jpg'),
+  require('../assets/house_5.jpg'),
+  require('../assets/house_6.jpg'),
+  require('../assets/house_7.jpg'),
+  require('../assets/house_8.jpg')
+]
+
+export default [
+  { 
+    id: 'asdfa0',
+    title: 'The Willows', 
+    img: images[0],
+    location: '3517 W. Gray St. Utica',
+    rooms: 3,
+    bathrooms: 2,
+    dimensions: '230 ft²',
+    price: 440
+  },
+  { 
+    id: 'asdfa1',
+    title: 'The Willows', 
+    img: images[1],
+    location: '3517 W. Gray St. Utica',
+    rooms: 3,
+    bathrooms: 2,
+    dimensions: '230 ft²',
+    price: 440
+  },
+  { 
+    id: 'asdfa2',
+    title: 'The Willows', 
+    img: images[2],
+    location: '3517 W. Gray St. Utica',
+    rooms: 3,
+    bathrooms: 2,
+    dimensions: '230 ft²',
+    price: 440
+  },
+  { 
+    id: 'asdfa3',
+    title: 'The Willows', 
+    img: images[3],
+    location: '3517 W. Gray St. Utica',
+    rooms: 3,
+    bathrooms: 2,
+    dimensions: '230 ft²',
+    price: 440
+  },
+  { 
+    id: 'asdfa4',
+    title: 'The Willows', 
+    img: images[4],
+    location: '3517 W. Gray St. Utica',
+    rooms: 3,
+    bathrooms: 2,
+    dimensions: '230 ft²',
+    price: 440
+  },
+  { 
+    id: 'asdfa5',
+    title: 'The Willows', 
+    img: images[5],
+    location: '3517 W. Gray St. Utica',
+    rooms: 3,
+    bathrooms: 2,
+    dimensions: '230 ft²',
+    price: 440
+  },
+  { 
+    id: 'asdfa6',
+    title: 'The Willows', 
+    img: images[6],
+    location: '3517 W. Gray St. Utica',
+    rooms: 3,
+    bathrooms: 2,
+    dimensions: '230 ft²',
+    price: 440
+  },
+  { 
+    id: 'asdfa7',
+    title: 'The Willows', 
+    img: images[7],
+    location: '3517 W. Gray St. Utica',
+    rooms: 3,
+    bathrooms: 2,
+    dimensions: '230 ft²',
+    price: 440
+  }
+]
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,24 +2,19 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, View, ScrollView } from 'react-native'
 import HouseCard from '../components/HouseCard'
+import data from '../data'
 
 export default function HomeScreen () {
   return (
     <View style={styles.container}>
-        <StatusBar style="auto" />
+      <StatusBar style="auto" />
         <ScrollView>
-            <HouseCard houseInfo={ 
-              { 
-                title: 'The Willows', 
-                img: require('../assets/house_1.jpg'),
-                location: '3517 W. Gray St. Utica',
-                rooms: 3,
-                bathrooms: 2,
-                dimensions: '230 ft²',
-                price: 440
-              } 
-            }/>
-        </ScrollView>
+          {
+              data.map(house => (
+                <HouseCard key={house.id} houseInfo={house}/>
+              ))
+          }
+      </ScrollView>
     </View>
   )
 }
