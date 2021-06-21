7617694ebbb8eb499c9db9b0fa2e25b7187f6001
Adan Mota Cruz <adanmotacruz17@gmail.com>
2021-06-21 09:23:57 -0500

    Data updated

--- src/data/index.js
+++ src/data/index.js
@@ -22,72 +22,72 @@ export default [
   },
   { 
     id: 'asdfa1',
-    title: 'The Willows', 
+    title: 'Robet Trust', 
     img: images[1],
-    location: '3517 W. Gray St. Utica',
-    rooms: 3,
-    bathrooms: 2,
-    dimensions: '230 ft²',
-    price: 440
+    location: '5216 Rich Street',
+    rooms: 7,
+    bathrooms: 5,
+    dimensions: '800 ft²',
+    price: 1000
   },
   { 
     id: 'asdfa2',
-    title: 'The Willows', 
+    title: 'Michel Ones', 
     img: images[2],
-    location: '3517 W. Gray St. Utica',
-    rooms: 3,
-    bathrooms: 2,
-    dimensions: '230 ft²',
-    price: 440
+    location: '8745 No Live H.',
+    rooms: 1,
+    bathrooms: 1,
+    dimensions: '50 ft²',
+    price: 200
   },
   { 
     id: 'asdfa3',
-    title: 'The Willows', 
+    title: 'Smith Where', 
     img: images[3],
-    location: '3517 W. Gray St. Utica',
+    location: '8751 Where I.',
     rooms: 3,
     bathrooms: 2,
     dimensions: '230 ft²',
-    price: 440
+    price: 450
   },
   { 
     id: 'asdfa4',
-    title: 'The Willows', 
+    title: 'Turtle Prince', 
     img: images[4],
-    location: '3517 W. Gray St. Utica',
-    rooms: 3,
-    bathrooms: 2,
-    dimensions: '230 ft²',
-    price: 440
+    location: '8549 Sour W.',
+    rooms: 5,
+    bathrooms: 4,
+    dimensions: '350 ft²',
+    price: 874
   },
   { 
     id: 'asdfa5',
-    title: 'The Willows', 
+    title: 'Following T.', 
     img: images[5],
-    location: '3517 W. Gray St. Utica',
+    location: '8549 New Aveniue',
     rooms: 3,
-    bathrooms: 2,
-    dimensions: '230 ft²',
-    price: 440
+    bathrooms: 4,
+    dimensions: '275 ft²',
+    price: 580
   },
   { 
     id: 'asdfa6',
-    title: 'The Willows', 
+    title: 'London N.', 
     img: images[6],
-    location: '3517 W. Gray St. Utica',
-    rooms: 3,
-    bathrooms: 2,
-    dimensions: '230 ft²',
-    price: 440
+    location: '5132 Nothern Bus',
+    rooms: 2,
+    bathrooms: 1,
+    dimensions: '120 ft²',
+    price: 300
   },
   { 
     id: 'asdfa7',
-    title: 'The Willows', 
+    title: 'Crossers', 
     img: images[7],
-    location: '3517 W. Gray St. Utica',
-    rooms: 3,
-    bathrooms: 2,
-    dimensions: '230 ft²',
-    price: 440
+    location: '547 Philip',
+    rooms: 4,
+    bathrooms: 1,
+    dimensions: '82 ft²',
+    price: 580
   }
 ]
